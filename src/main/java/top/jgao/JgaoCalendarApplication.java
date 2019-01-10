0c982651ba29156363cac5214185be70b0d03421
夏建高 <jiangao.xia.o>
2019-01-10 21:41:34 +0800

    test

--- src/main/java/top/jgao/JgaoCalendarApplication.java
+++ src/main/java/top/jgao/JgaoCalendarApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JgaoCalendarApplication {
 
     public static void main(String[] args) {
+        //test
         SpringApplication.run(JgaoCalendarApplication.class, args);
     }
 }
